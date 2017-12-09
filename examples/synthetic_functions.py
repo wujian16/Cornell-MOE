987874079dc4558474eb60d867563e95bf126758
Jian Wu <wujian046@gmail.com>
2017-12-08 21:24:50 -0800

    fix the bug in compute_variance

--- examples/synthetic_functions.py
+++ examples/synthetic_functions.py
@@ -7,7 +7,7 @@ class Branin(object):
         self._num_init_pts = 3
         self._sample_var = 0.0
         self._min_value = 0.397887
-        self._observations = numpy.arange(self._dim)
+        self._observations = []
         self._num_fidelity = 0
 
     def evaluate_true(self, x):
